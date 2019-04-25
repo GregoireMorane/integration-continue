8add29fd9be112146f0cba0c689a33c762a81d33
GrégoireMorane <gh.morane@gmail.com>
2019-04-25 16:10:32 +0200

    Deploy master branches to URL named after branch

---
+++ scripts/deploy_staging_app.sh
@@ -0,0 +1,3 @@
+echo "{ \"alias\": \"un-jour-je-serais-le-meilleur-dresseur\" }" > now-staging-app.json
+now --public --token $NOW_TOKEN
+now alias --token $NOW_TOKEN --local-config now-staging-app.json
