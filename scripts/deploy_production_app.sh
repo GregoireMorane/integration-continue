985c4520cd5bdaa3914df791b83206870d65cc75
GrégoireMorane <gh.morane@gmail.com>
2019-04-25 16:21:41 +0200

    Deploy tags to production

---
+++ scripts/deploy_production_app.sh
@@ -0,0 +1,3 @@
+echo "{ \"alias\": \"production-staging-un-jour-je-serais-le-meilleur-dresseur\" }" > now-production-app.json
+now --public --token $NOW_TOKEN
+now alias --token $NOW_TOKEN --local-config now-production-app.json
