e3bc8b7ad1f2b4475a7e394435fe307d5ddcd02e
GrégoireMorane <gh.morane@gmail.com>
2019-04-25 16:16:01 +0200

    ++

--- scripts/deploy_staging_app.sh
+++ scripts/deploy_staging_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"un-jour-je-serais-le-meilleur-dresseur\" }" > now-staging-app.json
+echo "{ \"alias\": \"staging-un-jour-je-serais-le-meilleur-dresseur\" }" > now-staging-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-staging-app.json
